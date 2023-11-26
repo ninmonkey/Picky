88064d2968cfe02e06d3b8f2f520c96f7fd6fb19
Jake Bolton <ninmonkeys@gmail.com>
2023-11-26 15:33:09 -0600

    new: using modulebuilder script

--- debug_harness.ps1
+++ debug_harness.ps1
@@ -1,5 +1,28 @@
 $modPath = (Join-Path $PSScriptRoot 'Picky.psm1')
-Import-Module -Force -PassThru $ModPath -DisableNameChecking
+$hasDotils = gcm 'Render.ModuleName' -m 'Dotils' -ea 'ignore'
+
+function RebuildModule.Picky {
+    pushd 'H:\data\2023\pwsh\PsModules\Picky' -StackName 'builder'
+    Build-Module
+    popd -StackName 'builder'
+}
+
+rebuildModule.Picky
+
+if($false) {
+    Import-Module -Force -PassThru $ModPath -DisableNameChecking
+} else {
+    if($HasDotils) {
+        Import-Module 'Picky' -Force -Passthru -DisableNameChecking
+            | Render.ModuleName
+    } else {
+        Import-Module 'Picky' -Force -Passthru -DisableNameChecking
+            | Join-String { $_.Name, $_.Version -join ' = ' }
+            | write-host -back 'darkorange' -fg 'black'
+
+    }
+
+}
 
 
 # Impo -pass -force -DisableName 'H:\data\2023\dotfiles.2023\pwsh\dots_psmodules\Dotils\Dotils.Fonts.psm1'
