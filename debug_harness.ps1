0b2b90a9ea45819484d08cd86fa843487e62d73f
Jake Bolton <ninmonkeys@gmail.com>
2023-11-26 16:37:56 -0600

    update debug_harness to support multiple import locations

--- debug_harness.ps1
+++ debug_harness.ps1
@@ -1,7 +1,52 @@
-$modPath = (Join-Path $PSScriptRoot 'Picky.psm1')
+$HarnessConfig = @{
+    Path = @{
+        PSModulesRoot = 'C:/Users/cppmo_000/SkyDrive/Documents/PowerShell/Modules' # find: what's the standard path?
+    }
+}
+
+Import-Module pansies -DisableNameChecking -PassThru
+[ValidateSet('FromLocal', 'FromModuleBuilder', 'FromInstall', 'Implicit', 'Picky')]
+$importMode =
+    'FromLocal'
+    # 'FromModuleBuilder'
+    # 'FromInstall'
+
+
+
+
+$newestModulePath = switch($ImportMode){
+    { $_ -in @('Implicit', 'Picky' ) } { 'Picky' }
+    'FromLocal' {
+        (Join-Path $PSScriptRoot 'Picky.psm1')
+        break }
+
+    'FromModuleBuilder' {
+            $newestBuild =
+                $getChildItemSplat = @{
+                    Recurse = $true
+                    Path = 'H:\data\2023\pwsh\PsModules\Picky\Picky\..\..\..\PsModules.Import\Picky'
+                    Filter = 'Picky.psm1'
+                }
+
+                Get-ChildItem @getChildItemSplat
+                | Sort-Object LastWriteTime -Descending -Top 1
+
+            $newestBuild
+                | Join-String -op 'newestBuild =: ' | Write-Host -back 'darkgreen'
+
+         break }
+    'FromInstall' {
+        gci $HarnessConfig.Path.PSModulesRoot Picky.psm1 -Depth 3
+             Sort-Object LastWriteTime -Descending -Top 1
+        break }
+    default  { throw "ShouldNeverReach: Unhandled mode: $ImportMode" }
+}
+
+# $modPath = (Join-Path $PSScriptRoot 'Picky.psm1')
 $hasDotils = gcm 'Render.ModuleName' -m 'Dotils' -ea 'ignore'
 
 function RebuildModule.Picky {
+    'building Picky ...' | write-host -back 'darkgreen'
     pushd 'H:\data\2023\pwsh\PsModules\Picky' -StackName 'builder'
     Build-Module
     popd -StackName 'builder'
@@ -10,13 +55,13 @@ function RebuildModule.Picky {
 rebuildModule.Picky
 
 if($false) {
-    Import-Module -Force -PassThru $ModPath -DisableNameChecking
+    Import-Module -Force -PassThru $newestModulePath -DisableNameChecking
 } else {
     if($HasDotils) {
-        Import-Module 'Picky' -Force -Passthru -DisableNameChecking
+        Import-Module $newestModulePath -Force -Passthru -DisableNameChecking
             | Render.ModuleName
     } else {
-        Import-Module 'Picky' -Force -Passthru -DisableNameChecking
+        Import-Module $newestModulePath -Force -Passthru -DisableNameChecking
             | Join-String { $_.Name, $_.Version -join ' = ' }
             | write-host -back 'darkorange' -fg 'black'
 
