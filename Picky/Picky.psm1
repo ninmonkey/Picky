0970e7e1e3c40414c5ce01beeb2e00513c0aca46
Jake Bolton <ninmonkeys@gmail.com>
2023-12-30 11:28:05 -0600

    new commands: ScriptBlock.GetInfo, Function.GetInfo

--- Picky/Picky.psm1
+++ Picky/Picky.psm1
@@ -60,7 +60,208 @@ if($ModuleConfig.TemplateFromCacheMe) {
             WriteBg $ColorBg ) -join ''
     }
 }
+function ScriptBlock.GetInfo {
+    <#
+    .SYNOPSIS
+        Quickly and easily grab properties and metadata for [ScriptBlock] types
+    .EXAMPLE
+        # use auto completion
+        Pwsh> gcm 'DoWork'
+            | Function.GetInfo ScriptBlock
+            | ScriptBlock.GetInfo File
+    .LINK
+        Picky\Function.GetInfo
+    .LINK
+        Picky\ScriptBlock.GetInfo
+    .notes
+        future info
+        - [ ] other scriptblock/function types
 
+        - [ ] DefaultParameterSet
+        - [ ] (Jsonify) => Id, Ast, Module, Etc...
+    #>
+    [OutputType(
+        'PSModuleInfo'
+
+    )]
+    [CmdletBinding(DefaultParameterSetName='FromPipe')]
+    param(
+        [Parameter( Mandatory, ParameterSetName='FromPipe',  ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Parameter( Mandatory, ParameterSetName='FromParam', Position = 0 )]
+        [Alias('Name', 'Func', 'Fn', 'Command', 'InObj', 'Obj', 'ScriptBlock', 'SB', 'E', 'Expression')]
+        [object]$InputObject,
+
+        [Parameter( ParameterSetName='FromPipe',  Position = 0 )]
+        [Parameter( ParameterSetName='FromParam', Position = 1 )]
+        [Parameter(Mandatory)]
+        [ValidateSet(
+            'Attributes',
+            'File',
+            'Module',
+            'StartPosition',
+            'Id',
+            'Ast'
+        )]
+        [string]$OutputKind
+    )
+    # future: assert properties exist
+    process {
+        if( $InputObject -isnot [ScriptBlock] ) {
+            'Expected A <ScriptBlock | ... >. Actual: {0}' -f @(
+                $InputObject.GetType().Name
+            )
+            | Dotils.Write-DimText | Infa
+                # | write-warning
+        }
+        'InputType: {0}, Expected <ScriptBlock>' -f @(
+            $InputObject.GetType().Name
+        ) | write-verbose
+
+        # if( -not $InputObject -isnot 'F')
+        if($Null -eq $InputObject) { return }
+        switch( $OutputKind ) {
+            'Attributes' {
+                # -is [List[Attribute]]
+                $result  = $Input.Attributes
+                break
+            }
+            'File' {
+                # -is [string]
+                $result  = $Input.File
+                break
+            }
+            'Module' {
+                # is [PSModuleInfo]
+                $result  = $input.Module
+                break
+            }
+            'StartPosition' {
+                # -is [PSToken]
+                $result  = $InputObject.StartPosition
+                break
+            }
+            'Id' {
+                # -is [Guid]
+                $result  = $InputObject.Id
+                break
+            }
+            'Ast' {
+                # -is [Ast>]
+                $result  = $InputObject.Ast
+                break
+            }
+            default { throw "Unhandled OutputKind: $OutputKind" }
+        }
+
+
+        $isBlank = [string]::IsNullOrWhiteSpace( $result )
+        if($isBlank -and $InputObject) {
+            'Object exists but the attribute is blank'
+                | Dotils.Write-DimText | Infa
+        }
+        return $result
+
+    }
+}
+function Function.GetInfo {
+    <#
+    .SYNOPSIS
+        Quickly and easily grab properties and metadata for [CommandInfo], [FunctionInfo] etc
+    .EXAMPLE
+        # use auto completion
+        Pwsh> gcm 'DoWork' | Function.GetInfo Parameters
+    .LINK
+        Picky\Function.GetInfo
+    .LINK
+        Picky\ScriptBlock.GetInfo
+    .notes
+        future info
+        - [ ] ParameterMetadata ResolveParameter(string name);
+        - [ ] DefaultParameterSet
+        - [ ] ScriptBlock
+        - [ ] CommandType
+
+        - [ ] (Jsonify) => Options, Description, Noun, Verb, Name, ModuleName, Source, Version
+    #>
+    [OutputType(
+        'ScriptBlock',
+        'PSModuleInfo',
+        '[IDictionary[string, [System.Management.Automation.ParameterMetadata]]]',
+        '[Collections.ObjectModel.ReadOnlyCollection[Management.Automation.CommandParameterSetInfo]]'
+
+    )]
+    [CmdletBinding(DefaultParameterSetName='FromPipe')]
+    param(
+        [Parameter( Mandatory, ParameterSetName='FromPipe',  ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [Parameter( Mandatory, ParameterSetName='FromParam', Position = 0 )]
+        [Alias('Name', 'Func', 'Fn', 'Command', 'InObj', 'Obj', 'ScriptBlock', 'SB')]
+        [object]$InputObject,
+
+        [Parameter( ParameterSetName='FromPipe',  Position = 0 )]
+        [Parameter( ParameterSetName='FromParam', Position = 1 )]
+        [Parameter(Mandatory)]
+        [ValidateSet(
+            'Attributes',
+            'ScriptBlock',
+            'Module',
+            'Parameters',
+            'ParameterSets'
+        )]
+        [string]$OutputKind
+    )
+    # future: assert properties exist
+    process {
+        if( $InputObject -isnot [CommandInfo] -and $InputObject -isnot [FunctionInfo]) {
+            'Expected A <CommandInfo | FunctionInfo>. Actual: {0}' -f @(
+                $InputObject.GetType().Name
+            )
+            | Dotils.Write-DimText | Infa
+                # | write-warning
+        }
+        'InputType: {0}, Expected <CommandInfo|FunctionInfo>' -f @(
+            $InputObject.GetType().Name
+        ) | write-verbose
+
+        # if( -not $InputObject -isnot 'F')
+        if($Null -eq $InputObject) { return }
+        switch( $OutputKind ) {
+            'Attributes' {
+                $result  = $Input.ScriptBlock.Attributes
+                break
+            }
+            'ScriptBlock' {
+                # -is [ScriptBlock]
+                $result  = $Input.ScriptBlock
+                break
+            }
+            'Module' {
+                # is [PSModuleInfo]
+                $result  = $input.Module
+                break
+            }
+            'Parameters' {
+                # -is [Dictionary<string, ParameterMetadata>]]
+                $result  = $InputObject.Parameters
+                break
+            }
+            'ParameterSets' {
+                # -is [ReadOnlyCollection<CommandParameterSetInfo>]
+                $result  = $InputObject.ParameterSets
+                break
+            }
+            default { throw "Unhandled OutputKind: $OutputKind" }
+        }
+
+
+        $isBlank = [string]::IsNullOrWhiteSpace( $result )
+        if($isBlank -and $InputObject) {
+            'Object exists but the attribute is blank'
+                | Dotils.Write-DimText | Infa
+        }
+        return $result
+
+    }
+}
 function String.GetCrumbs {
     <#
     .SYNOPSIS
