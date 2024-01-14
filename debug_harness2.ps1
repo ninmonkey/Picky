d0cc7f48a1d22daa78151b33454d02f65557dbc9
Jake Bolton <ninmonkeys@gmail.com>
2024-01-14 17:44:45 -0600

    debug sketch

--- debug_harness2.ps1
+++ debug_harness2.ps1
@@ -1,3 +1,51 @@
 err -clear
 Import-Module -force -passthru (Join-Path $PSScriptRoot './Picky/Picky.psm1')
     | Render.ModuleName
+
+hr
+'sdf'.EndsWith
+hr
+
+$regex = @{}
+$regex.ParamBindLog_SingleLine = @'
+(?x)
+    ^
+    (?<Source>.*?)
+    \s
+    (?<Severity>\S*)
+    :\s
+    (?<SomeId>\d+)
+    \s:
+    (?<RawMessage>
+        (?<Indent>\s+)
+        (?<Message>.*)
+    )
+    (?<Rest>.*?)
+    $
+'@
+
+gc $Log_ParamBind -Raw | StripAnsi | CountOf | SPlit.NL | %{
+    if( $_ -match $Regex.ParamBindLog_SingleLine ) {
+        $matches.remove(0)
+        $found = [pscustomobject]$matches
+    }
+    $found
+}| ft Source, Severity, Message
+
+Str.EndsWith -in 'foo.NL' -SubStr '.nl' # -UsingCaseSensitive -culture 'en-us'
+
+$Log_ParamBind = gi (Join-path temp: 'paramBind.cs')
+$Log_ParamBind ??= (Join-path temp: 'paramBind.cs')
+
+# return
+Clear-Content -Path $Log_ParamBind
+Trace-Command -Name '*Param*' -FilePath $Log_ParamBind -Expression {
+    Str.EndsWith -in 'a.NL' -SubString '.nl'
+} # it appends
+$log_paramBind | join-string -f 'wrote: {0}' | write-verbose -verb
+return
+'a.NL' | Str.EndsWith -SubStr '.nl' # -UsingCaseSensitive -culture 'en-us'
+
+# Str.EndsWith -InputText '.NL' -Substring '.nl' -UsingCaseSensitive -Culture 'en-us'
+# Str.EndsWith -InputText '.NL' -Substring '.nl' -ComparisonType InvariantCultureIgnoreCase
+return
--- debug_harness3.ps1
+++ debug_harness3.ps1
@@ -1,6 +1,102 @@
 
 $t1 = [int]
-[Func`].MakeGenericType($t1, [string])
+[Func`2].MakeGenericType($t1, [string])
+
+
+
+
+function dbg.ProcessParamLog {
+    param(
+        [string]$Path,
+        [string]$TemplateName
+    )
+    $FulLName = Get-Item -ea 'stop' $Path
+    $re = @'
+(?x)
+        ^
+        (?<Source>.*?)
+        \s
+        (?<Severity>\S*)
+        :\s
+        (?<SomeId>\d+)
+        \s:
+        (?<RawMessage>
+            (?<Indent>\s+)
+            (?<Message>.*)
+        )
+        (?<Rest>.*?)
+        $
+'@
+
+        function __fmtLineTemplate {
+            param(
+                [Parameter(Mandatory)]
+                [object]$InputObject,
+
+                [Parameter(Mandatory)]
+                [ArgumentCompletions(
+                    'Default',
+                    'A'
+                )]
+                [string]$Template = 'Default'
+            )
+            $T = $InputObject
+            switch($Template) {
+                'A' {
+                    Join-String -In $T -p { @(
+                        $_.Severity
+                        $_.SOmeId
+                        $_.Message
+
+                    ) -join ''}
+                }
+                'Predent' {
+                   $t | Join-String -p {@(
+                        $_.Source | Join.Predent -Depth 0
+                        $_.Severity | Join.Predent -Depth 1
+                        $_.Message  | Join.Predent -Depth 2
+                            ) -join "`n" }
+                }
+                '::' {@(
+                    $t.Severity,
+                    $t.RawMessage -join '::'
+                ) -join ''}
+                'PassThru' {
+                    $t
+                }
+                default {
+                    throw "Unhandled template: $Template"
+                }
+            }
+        }
+
+
+    $TemplateName | Join-String -op 'End: Template: '
+        | write-verbose -Verbose
+
+    gc -Raw $FulLName | StripAnsi | split.NL
+        | %{
+            if( $_ -match $Re ) { [pscustomobject]$Matches }
+        } | Join-String -sep "`n" -p {
+            __fmtLineTemplate -In $_ -Template $TemplateName
+        }
+
+    gc $FullLogPath | OutNull -Extra 'raw line count'
+}
+
+$FullLogPath = gi 'C:\Users\cppmo_000\AppData\Local\Temp\ParamBinding.log.raw'
+
+dbg.ProcessParamLog -path $FullLogPath -Template 'Default'
+function tryF {
+    param( [string]$Template )
+    dbg.ProcessParamLog -path $FullLogPath -Template $Template
+
+}
+
+
+
+
+
 
 
 return
